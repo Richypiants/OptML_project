0bc7f3a8156b3882ae6a521bbddec9f6f05a9319
HermanEfr <86071306+HermanEfr@users.noreply.github.com>
2025-06-13 15:36:58 +0200

    Add files via upload

--- strategies.py
+++ strategies.py
@@ -41,7 +41,7 @@ def halving_frequency(state):
         return False
 
 # The frequency gets doubled at each update of the preconditioner, making the updates more and more frequent as the training progresses
-def doubling_frequency(state):
+def doubling_frequency1024(state):
     global counter
     if "last_update" not in state:
         state["last_update"] = 0
@@ -50,14 +50,14 @@ def doubling_frequency(state):
 
     if state['step'] - state['last_update'] >= state['precondition_frequency']:
         state['last_update'] = state['step']
-        state['precondition_frequency'] = state['precondition_frequency']*2  # Clip the frequency to max 1024
+        state['precondition_frequency'] = min(1024, state['precondition_frequency']*2)  # Clip the frequency to max 1024
         counter += 1
         return True
     else:
         return False
 
 # Same as the previous, but with a tighter clip as the maximum frequency
-def doubling_frequency_clipped(state):
+def doubling_frequency_256(state):
     global counter
     if "last_update" not in state:
         state["last_update"] = 0
@@ -66,37 +66,12 @@ def doubling_frequency_clipped(state):
 
     if state['step'] - state['last_update'] >= state['precondition_frequency']:
         state['last_update'] = state['step']
-        state['precondition_frequency'] = min(256, state['precondition_frequency']*2)  # Clip the frequency to max 1024
+        state['precondition_frequency'] = min(256, state['precondition_frequency']*2)  # Clip the frequency to max 256
         counter += 1
         return True
     else:
         return False
 
-# The frequency gets doubled at each update of the preconditioner up to a certain threshold,
-# and then gets halved down to 1 once again, leading to a lot of updates at both start and end of training but not in the middle
-def doubling_then_halving_frequency(state):
-    global counter
-    if "last_update" not in state:
-        state['last_update'] = 0
-        state['last_freq'] = 1
-        state['halving'] = False
-        counter = 1
-        return True
-
-    if state['last_freq'] >= state['precondition_frequency']:
-        state['halving'] = True
-
-    if state['step'] - state['last_update'] >= state['last_freq']:
-        state['last_update'] = state['step']
-        if state['halving']:
-            state['last_freq'] = max(1, state['last_freq']//2)
-            counter += 1
-        else:
-            state['last_freq'] = min(1024, state['last_freq']*2)
-            counter += 1
-        return True
-    else:
-        return False
 
 # A fixed interval of 256 iterations is defined, and the frequency gets doubled at the end of each interval
 def fixed_interval_doubling_frequency(state):
